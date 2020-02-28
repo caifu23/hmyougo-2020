99ea530731bdb145eefdfdd299421815f19d7729
caifu23 <1224758467@qq.com>
2020-02-28 17:00:24 +0800

    03 - 渲染轮播图、导航菜单、搜索框，以及相应数据获取

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,54 @@
 //index.js
 //获取应用实例
 const app = getApp()
-import { request } from '../../utils/request.js'
+import { request, get } from '../../utils/request.js'
 Page({
+  data: {
+    // 轮播图
+    banners: [],
+    // 导航菜单
+    menus: []
+  },
   onLoad() {
-    // 测试request
-    request({
+  //  获取轮播图
+  this.getBanners()
+  // 获取导航菜单
+    this.getMeun(0)
+  },
+  // 获取轮播图
+  getBanners() {
+    get({
       url: '/home/swiperdata'
-    }).then( res => {
+    }).then(res => {
+      // console.log(res)
+      const { message } = res.data
+      if(!message) return;
+      this.setData({
+        banners: message
+      })
+    })
+  },
+  // 获取导航菜单
+  getMeun() {
+    get({
+      url:'/home/catitems'
+    }).then(res => {
       console.log(res)
+      let { message } = res.data
+      if(!message) return;
+      // 改变跳转链接
+      message = message.map(val => {
+        if(val.name === '分类') {
+          val.url = '/pages/category/index'
+        }else {
+          val.open_type = "navigate"
+        }
+        return val
+      })
+      // 赋值给 menus
+      this.setData({
+        menus: message
+      })
     })
   }
 })
