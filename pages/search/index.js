fdd0b4ce5c1f3d1120afd92084ed6de0ef506be7
caifu23 <1224758467@qq.com>
2020-03-01 16:58:12 +0800

    09 -  搜索页，输入框搜索词确认后，存到搜索历史，并在输入框为空时显示搜索历史，实现搜索历史本地存储和删除

--- pages/search/index.js
+++ pages/search/index.js
@@ -9,14 +9,18 @@ Page({
   data: {
     query: '',
     productList: [],
-    currentCondition: 0
+    currentCondition: 0,
+    priceUpDown: 0,  // 0 代表降序 1 代表升序
+    queryArr: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取历史搜索
+    this.getHistory()
+    console.log(11)
   },
 
   // 搜索框输入时
@@ -28,6 +32,22 @@ Page({
       query: value
     })
     this.searchProductKey()
+  },
+
+  // 搜索框确认时, 存储 搜索历史
+  handlerConfirm(e) {
+    // console.log(e)
+    let { value } = e.detail
+    let orgArr = this.data.queryArr
+    orgArr.unshift(value)
+    this.setData({
+      queryArr: orgArr
+    })
+    // 存储到本地
+    wx.setStorage({
+      key: "searchHistory",
+      data: JSON.stringify(orgArr)
+    })
   },
 
   // 根据关键词 搜索商品
@@ -45,6 +65,50 @@ Page({
       })
     })
   },
+
+  // 点击筛选条件时
+  handlerCondition(e) {
+    let { condition } = e.currentTarget.dataset
+    this.setData({
+      currentCondition: condition
+    })    
+  },
+
+  // 点击 切换价格升降序
+  handlerPrice() {
+    let rate = this.data.priceUpDown === 0 ? 1 : 0
+    this.setData({
+      priceUpDown: rate
+    })
+  },
+
+  // 获取搜索历史
+  getHistory() {
+    wx.getStorage({
+      key: 'searchHistory',
+      success: (res)=> {
+        // console.log(res.data)
+        let history = JSON.parse(res.data)
+        // console.log(history)
+        this.setData({
+          queryArr: history
+        })
+      }
+    })
+  },
+
+  // 删除搜索历史
+  delHistory() {
+    wx.removeStorage({
+      key: 'searchHistory',
+      success: (res) => {
+        console.log(res)
+      }
+    })
+    this.setData({
+      queryArr: []
+    })
+  }
 
   
 })
