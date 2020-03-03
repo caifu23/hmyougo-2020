82b43c9b7c6256e598fa4fe615e5b1315eb02031
caifu23 <1224758467@qq.com>
2020-03-03 23:10:01 +0800

    26 -  新商品列表页- 引入search组件，使用组件外部样式类和数据传递

--- components/search/index.js
+++ components/search/index.js
@@ -4,8 +4,18 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    query: {
+      type: String,
+      value: '搜索'
+    },
+    align: {
+      type: String,
+      value: 'middle'
+    }
   },
+
+  // 外部样式类
+  externalClasses: ['box', 'box-in'],
 
   /**
    * 组件的初始数据
