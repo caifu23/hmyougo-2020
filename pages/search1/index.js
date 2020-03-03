c40e7da3c36371f1d5871204b5b02fd32cdef54f
caifu23 <1224758467@qq.com>
2020-03-03 14:13:10 +0800

    18 -  新搜索页-  实现静态布局

---
+++ pages/search1/index.js
@@ -0,0 +1,18 @@
+// pages/search1/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    showRecommand: false,  // 是否隐藏搜索建议
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  }
+  
+})
