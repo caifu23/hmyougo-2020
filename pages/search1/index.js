57a8bf63cd7c216647c4d827fb51a1fc8e9ca515
caifu23 <1224758467@qq.com>
2020-03-03 17:47:16 +0800

    19 -  新搜索页-  监听搜索框的输入，并请求输入建议

--- pages/search1/index.js
+++ pages/search1/index.js
@@ -1,4 +1,6 @@
 // pages/search1/index.js
+import { get, post } from '../../utils/request.js'
+
 Page({
 
   /**
@@ -6,6 +8,8 @@ Page({
    */
   data: {
     showRecommand: false,  // 是否隐藏搜索建议
+    inputValue: '',  // 输入框的值
+    recommendList: [], //输入建议
   },
 
   /**
@@ -13,6 +17,43 @@ Page({
    */
   onLoad: function (options) {
 
+  },
+
+  // 监听键盘输入
+  handlerInput(e) {
+
+    let { value } = e.detail
+    // 将输入值 保存
+    this.setData({
+      inputValue: value
+    })
+
+    // 请求 输入建议
+    this.getRecommendKey()
+  },
+
+  // 请求关键词 输入建议
+  getRecommendKey() {
+    get({
+      url: '/goods/qsearch',
+      data: {
+        query: this.data.inputValue
+      }
+    }).then(res => {
+
+      let { message } = res.data
+      if(message.length === 0) return;
+      
+      // 如果数组长度多余15,截取后半部分
+      if(message.length > 15) {
+        message.length = 15
+      }
+      // 保存 输入建议
+      this.setData({
+        recommendList: message
+      })
+
+    })
   }
   
 })
