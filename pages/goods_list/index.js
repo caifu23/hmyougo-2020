cab3a1c05a15175d1dae8cba86a3544e9acfa716
caifu23 <1224758467@qq.com>
2020-03-04 17:34:17 +0800

    32 -  新商品列表页-  商品列表布局和数据渲染

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,6 @@
 // pages/goods_list/index.js
+import { get, post } from '../../utils/request.js'
+
 Page({
 
   /**
@@ -7,13 +9,26 @@ Page({
   data: {
     currentCondition: 0,
     upDown: true, // 价格的升降
+    query: '', //查询关键字
+    goodsList: [], //商品列表
+    total: 0,  //商品总数
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 获取查询关键字
+    let { query } = options
+    // 保存
+    this.setData({
+      query: query
+    })
 
+    // 获取商品列表数据
+    this.getGoodsList({
+      query: query
+    })
   },
 
   // 切换条件
@@ -32,6 +47,30 @@ Page({
         upDown: !this.data.upDown
       })
     }
+  },
+
+  // 获取商品列表数据
+  getGoodsList(options) {
+    get({
+      url: '/goods/search',
+      data: options
+    }).then(res => {
+      console.log(res)
+      // 获取商品数组，总数
+      let { goods, total } = res.data.message
+
+      // 修改价格, 保留2位小数
+      goods = goods.map(v => {
+        v.goods_price = Number(v.goods_price).toFixed(2)
+        return v;
+      }) 
+
+      // 保存
+      this.setData({
+        goodsList: goods,
+        total: total
+      })
+    })
   }
 
   
