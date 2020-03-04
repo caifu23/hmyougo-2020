16a34171159e0434e01c963cf3be7f2202af9307
caifu23 <1224758467@qq.com>
2020-03-04 13:24:55 +0800

    27 -  商品详情页 - 轮播图大图预览

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -13,6 +13,7 @@ Page({
     autoplay: false, // 是否自动切换
     interval: 2000, // 切换时间间隔
     duration: 500,  // 滑动动画时长
+    picUrls: [],  // 预览图url数组
   },
 
   /**
@@ -38,13 +39,40 @@ Page({
         goods_id: this.data.goods_id
       }
     }).then(res => {
-      console.log(res)
+      // console.log(res)
       let { message } = res.data
       // 保存商品数据
       this.setData({
         goods_data: message
       })
+      // 处理预览图片数据
+      this.modPreviewData()
     })
+  },
+
+  // 处理图片预览图数据
+  modPreviewData() {
+    let { pics } = this.data.goods_data
+    // 将图片url作为数组
+    let newPics = pics.map(v => {
+      return v.pics_big_url
+    })
+    // 保存
+    this.setData({
+      picUrls: newPics
+    })
+  },
+
+  // 点击轮播图，实现全屏预览
+  previewImg(e) {
+    // 获取当前点击的 图片索引
+    let { index } = e.currentTarget.dataset
+    // 全屏预览图片
+    wx.previewImage({
+      current: this.data.picUrls[index], // 当前显示图片的http链接
+      urls: this.data.picUrls // 需要预览的图片http链接列表
+    })
+    
   }
 
 })
