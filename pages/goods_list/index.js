4524d57dc5b3b273a6908fd700339c39385de880
caifu23 <1224758467@qq.com>
2020-03-04 17:04:22 +0800

    31 -  新商品列表页-  头部筛选条件布局和切换样式

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    currentCondition: 0,
+    upDown: true, // 价格的升降
   },
 
   /**
@@ -14,6 +15,24 @@ Page({
   onLoad: function (options) {
 
   },
+
+  // 切换条件
+  changeCondition(e) {
+    let { index } = e.currentTarget.dataset
+    // 判断点击是否当前的
+    if (this.data.currentCondition !== index) {
+      // 修改当前筛选条件
+      this.setData({
+        currentCondition: index
+      })
+    }
+    // 点击为价格时，切换升降
+    if (index === 2) {
+      this.setData({
+        upDown: !this.data.upDown
+      })
+    }
+  }
 
   
 })
