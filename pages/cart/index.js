1cc99b0ca8d444d4d9beb247d9cc58cb76a02c2c
caifu23 <1224758467@qq.com>
2020-03-06 17:28:35 +0800

    51 - 购物车页 - 改变数量时，如果数量1，给用户提示是否删除； 优化数量加减代码

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -112,6 +112,108 @@ Page({
     wx.setStorageSync('cartList', cartData)
   },
 
+  // 改变商品数量
+  changeNumber(e) {
+    // index 被点击的索引  ，num是加减号的 对应数字(-1, 1)
+    let { index, num } = e.currentTarget.dataset
+    let cartData =this.data.cartData
+    // 判断当前数量为1，并且是减号时，
+    // 提示用户是否删除该商品
+    if (num === -1 && cartData[index].num === 1) {
+      // 对话框
+      wx.showModal({
+        title: '提示',
+        content: '确认将该商品删除吗？',
+        success: (res) => {
+          if (res.confirm) {
+            // 用户点击了确定，则直接删除该商品
+            cartData.splice(index, 1)
+            // 保存
+            this.setData({
+              cartData: cartData
+            })
+            // 重新结算总价格
+            this.computePrice()
+          }
+        }
+      })
+    }else {
+      
+      // 给该商品数量加减一
+      cartData[index].num += num
+      // 保存
+      this.setData({
+        cartData: cartData
+      })
+      // 重新结算总价格
+      this.computePrice()
+    }
+
+    
+  },
+
+  // 商品数量，直接输入
+  inputNum(e) {
+    // 购物车商品 第几项：index
+    let { index } = e.currentTarget.dataset
+    // 购物车
+    let cartData = this.data.cartData
+    // 输入框的值
+    let { value } = e.detail  
+
+    // 判断输入值为空，则不继续修改
+    // 并恢复原先的值 
+    // setData可以触发 页面数据重新渲染
+    if (value === '') {
+      this.setData({
+        cartData: cartData
+      })
+      return ;
+    }
+
+    // 如果输入值为0，则询问用户是否删除
+    if(+value === 0) {
+      wx.showModal({
+        title: '提示',
+        content: '确认将该商品删除吗？',
+        success: (res) => {
+          if (res.confirm) {
+            // 确认：删除该商品
+            cartData.splice(index, 1)
+          } else if (res.cancel) {
+            // 取消：数量改为 1
+            cartData[index].num = 1
+          }
+          // 保存
+          this.setData({
+            cartData: cartData
+          })
+          // 计算总数
+          this.computePrice()
+        }
+      })
+      return ;
+    }
+
+    value = +value  // 转number类型
+    
+    // 商品数量改变
+    cartData[index].num = value
+
+    // 保存， 触发页面数据  重新渲染
+    this.setData({
+      cartData: cartData
+    })
+    wx.setStorageSync('cartList', cartData)
+
+    // 如果该商品是 勾选的,则计算总价格;  反之,不用
+    // 计算总价格\总件数
+    if (cartData[index].selectStatus) {
+      this.computePrice()
+    }
+    
+  },
+
   // 切换勾选状态
   changeSelect(e) {
     let { index } = e.currentTarget.dataset
@@ -175,10 +277,10 @@ Page({
 
   // 计算合计金额、结算件数
   computePrice() {
-    let cartData = [...this.data.cartData]
+    let cartData = this.data.cartData
     let price = 0
     let number = 0
-    // 遍历  累加
+    // 遍历  累加选中状态的商品
     cartData.forEach(v => {
       if (v.selectStatus) {
         price += v.goods_price * v.num
@@ -191,6 +293,8 @@ Page({
       totalPrice: price,
       totalNum: number
     })
+    // 本地存储
+    wx.setStorageSync('cartList', cartData)
   }
 
 
